25e9f1d8ac9da776ef6caa84183ee17b088aed6d
meerabdulkadir12 <165972800+meerabdulkadir12@users.noreply.github.com>
2024-11-07 12:32:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from sklearn.datasets import load_iris
+st.write('hello world')
+st.write('GM')
+st.balloons()
+
+data = load_iris(as_frame = True).frame
+st.write(data.head())
